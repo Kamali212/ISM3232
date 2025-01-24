22ace533f5a3b577fe14ecaf1ef7468d80ee13a3
Kamali <Kamalikh3@gmail.com>
2025-01-24 15:49:05 -0500

    Task 2 Product Details

---
+++ cc_1.js
@@ -0,0 +1,19 @@
+// Task 1 Variables
+let employeeName = "Padme Amidala"
+const employeeID = 0
+var isActive = true
+console.log(typeof employeeName);
+console.log(typeof employeeID);
+console.log(typeof isActive);
+
+// Task 2 Primitive Data Tyoes
+let productName = "Boots"
+const productPrice = 650
+var productquantity = 13
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof productquantity);
+
+
+
+
