1db4ff0bfc0efb9c382b2de9f60f0ca99561dfc3
Kamali <Kamalikh3@gmail.com>
2025-01-24 16:48:17 -0500

    Task 4 Customer Messaging

--- cc_1.js
+++ cc_1.js
@@ -1,19 +1,33 @@
 // Task 1 Variables
-let employeeName = "Padme Amidala"
-const employeeID = 0
-var isActive = true
-console.log(typeof employeeName);
-console.log(typeof employeeID);
-console.log(typeof isActive);
+let employeeName = "Padme Amidala"; // assigning a variable for employees name and assigning it a string value
+const employeeID = 0; // assigning a variable for employee id and assigning it a numeric value
+var isActive = true; // assigning a variable for employee status and assigning it a boolean value
+console.log(typeof employeeName); //logging employees name and string type to the console
+console.log(typeof employeeID); // logging the employee id and string type to the console
+console.log(typeof isActive); // logging employee status and string type to the console
 
 // Task 2 Primitive Data Tyoes
-let productName = "Boots"
-const productPrice = 650
-var productquantity = 13
-console.log(typeof productName);
-console.log(typeof productPrice);
-console.log(typeof productquantity);
+let productName = "Boots"; // assigning a variable for product name and assigning it a string value
+const productPrice = 650; // assigning a variable for product price and assigning it a numeric value
+var productquantity = 13; // assigning a variable for product quantity and assigning it a numeric value
+console.log(typeof productName); // logging product name and string type to the console
+console.log(typeof productPrice); // logging product price and string type to the console
+console.log(typeof productquantity); // logging product quantity and string type to the console
 
+// Task 3 Number Data Type
 
+let accountBalance = 13000; // assigning a variable for account balance and assigning it a numeric value
+accountBalance += 200; // adding a numeric value to account balance
+console.log(accountBalance); // logging account balance after addition
+accountBalance -=1000; // subtracting numeric value from account balance
+console.log(accountBalance); // logging account balance after subtraction
+accountBalance *= 2; // multiplying balance by 2
+console.log(accountbalance) // logging account balance after multiplication
+accountBalance /=2; // dividing balance by 2
+consolelog(accountbalance); // logging account balance after division
 
+// Task 4 String Data Type
 
+let customername = "Luke Skywalker"; // assigning a variable for customer name and assigning it a string value
+let greeting = "Welcome, " + customername + "!"; // assigning a variable for greeting 
+consolelog(greeting); // logging greeting 
